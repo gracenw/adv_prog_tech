d70b1afd856b9e24b0aa2be4bc8861fa82082242
gracenw <gracen.wallace@gatech.edu>
2021-10-06 15:32:46 -0400

    finished problem 1

--- lab2/Lab2_Problem1.cpp
+++ lab2/Lab2_Problem1.cpp
@@ -20,7 +20,7 @@
 using namespace std;
 
 /* total number of moves, shared between threads */
-atomic_int totalMoves(0);
+atomic_uint64_t totalMoves(0);
 
 /* struct for holding ant's placement, it has a seed, and number of moves */
 struct laboringAnt 
@@ -33,12 +33,12 @@ struct laboringAnt
   function: laboriousAnt()
   
 */
-void laboriousAnt(const unsigned numRuns)
+void laboriousAnt (const unsigned numRuns)
 {
     for (int n = 0; n < numRuns; n ++)
     {
         /* create an ant */
-        struct laboringAnt ant = { 2, 2, 0, false };
+        struct laboringAnt ant = { 2, 2, -1, false };
 
         /* random number generation tools */
         default_random_engine generator;
@@ -60,8 +60,66 @@ void laboriousAnt(const unsigned numRuns)
             /* increment number of moves */ 
             ant.numMoves ++;
 
-            /* sides & corners */
-            if (ant.x == 0 || ant.x == 4 || ant.y == 0 || ant.y == 4)
+            /* move based on current location */
+            if (ant.y == 0)
+            {
+                /* pick up seed in bottom row */
+                if (!ant.hasSeed && botRowSeeds.find(ant.x) != botRowSeeds.end())
+                {
+                    botRowSeeds.erase(ant.x);
+                    ant.hasSeed = true;
+                }
+
+                if (ant.x == 0)
+                {
+                    nextMove = twoMoves(generator);
+                    switch(nextMove) 
+                    {
+                        case 0: /* up */
+                            ant.y ++;
+                            break;
+                        case 1: /* right */
+                            ant.x ++;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else if (ant.x == 4)
+                {
+                    nextMove = twoMoves(generator);
+                    switch(nextMove) 
+                    {
+                        case 0: /* up */
+                            ant.y ++;
+                            break;
+                        case 1: /* left */
+                            ant.x --;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    nextMove = threeMoves(generator);
+                    switch(nextMove) 
+                    {
+                        case 0: /* up */
+                            ant.y ++;
+                            break;
+                        case 1: /* left */ 
+                            ant.x --;
+                            break;
+                        case 2: /* right */
+                            ant.x ++;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            else if (ant.y == 4)
             {
                 /* drop seed in top row */
                 if (ant.hasSeed && ant.y == 4 && topRowSeeds.find(ant.x) == topRowSeeds.end())
@@ -70,194 +128,117 @@ void laboriousAnt(const unsigned numRuns)
                     ant.hasSeed = false;
                 }
 
-                /* pick up seed in bottom row */
-                if (!ant.hasSeed && ant.y == 0 && botRowSeeds.find(ant.x) != botRowSeeds.end())
-                {
-                    botRowSeeds.erase(ant.x);
-                    ant.hasSeed = true;
-                }
-
-                /* check if in a corner */
+                if (ant.x == 0)
                 {
                     nextMove = twoMoves(generator);
-                    
-                    if (ant.x == 0 && ant.y == 4)
+                    switch(nextMove) 
                     {
-                        switch(nextMove) 
-                        {
-                            case 0: /* down */
-                                ant.y --;
-                                break;
-                            case 1: /* right */
-                                ant.x ++;
-                                break;
-                            default:
-                                break;
-                        }
-
-                        continue;
-                    }
-                    if (ant.x == 4 && ant.y == 4)
-                    {
-                        switch(nextMove) 
-                        {
-                            case 0: /* down */ 
-                                ant.y --;
-                                break;
-                            case 1: /* left */
-                                ant.x --;
-                                break;
-                            default:
-                                break;
-                        }
-
-                        continue;
-                    }
-                    if (ant.x == 0 && ant.y == 0)
-                    {
-                        switch(nextMove) 
-                        {
-                            case 0: /* up */
-                                ant.y ++;
-                                break;
-                            case 1: /* right */
-                                ant.x ++;
-                                break;
-                            default:
-                                break;
-                        }
-
-                        continue;
-                    }
-                    if (ant.x == 4 && ant.y == 0)
-                    {
-                        switch(nextMove) 
-                        {
-                            case 0: /* up */
-                                ant.y ++;
-                                break;
-                            case 1: /* left */
-                                ant.x --;
-                                break;
-                            default:
-                                break;
-                        }
-
-                        continue;
+                        case 0: /* down */
+                            ant.y --;
+                            break;
+                        case 1: /* right */
+                            ant.x ++;
+                            break;
+                        default:
+                            break;
                     }
                 }
-
-                /* not in a corner but still on a side */
+                else if (ant.x == 4)
+                {
+                    nextMove = twoMoves(generator);
+                    switch(nextMove) 
+                    {
+                        case 0: /* down */ 
+                            ant.y --;
+                            break;
+                        case 1: /* left */
+                            ant.x --;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
                 {
                     nextMove = threeMoves(generator);
-
-                    /* bottom row */
-                    if (ant.y == 0)
+                    switch(nextMove) 
                     {
-                        switch(nextMove) 
-                        {
-                            case 0: /* up */
-                                ant.y ++;
-                                break;
-                            case 1: /* left */ 
-                                ant.x --;
-                                break;
-                            case 2: /* right */
-                                ant.x ++;
-                                break;
-                            default:
-                                break;
-                        }
-
-                        continue;
-                    }
-                    /* top row */
-                    if (ant.y == 4)
-                    {
-                        switch(nextMove) 
-                        {
-                            case 0: /* down */
-                                ant.y --;
-                                break;
-                            case 1: /* left */
-                                ant.x --;
-                                break;
-                            case 2: /* right */
-                                ant.x ++;
-                                break;
-                            default:
-                                break;
-                        }
-
-                        continue;
-                    }
-                    /* far left column */
-                    if (ant.x == 0)
-                    {
-                        switch(nextMove) 
-                        {
-                            case 0: /* right */
-                                ant.x ++;
-                                break;
-                            case 1: /* up */
-                                ant.y ++;
-                                break;
-                            case 2: /* down */
-                                ant.y --;
-                                break;
-                            default:
-                                break;
-                        }
-
-                        continue;
-                    }
-                    /* far right column */
-                    if (ant.x == 4)
-                    {
-                        switch(nextMove) 
-                        {
-                            case 0: /* left */
-                                ant.x --;
-                                break;
-                            case 1: /* up */
-                                ant.y ++;
-                                break;
-                            case 2: /* down */
-                                ant.y --;
-                                break;
-                            default:
-                                break;
-                        }
-
-                        continue;
+                        case 0: /* down */
+                            ant.y --;
+                            break;
+                        case 1: /* left */
+                            ant.x --;
+                            break;
+                        case 2: /* right */
+                            ant.x ++;
+                            break;
+                        default:
+                            break;
                     }
                 }
             }
-            /* middle */
             else 
             {
-                nextMove = fourMoves(generator);
-
-                switch(nextMove) 
+                if (ant.x == 0)
                 {
-                    case 0: /* up */
-                        ant.y ++;
-                        break;
-                    case 1: /* down */
-                        ant.y --;
-                        break;
-                    case 2: /* left */
-                        ant.x --;
-                        break;
-                    case 3: /* right */
-                        ant.x ++;
-                        break;
-                    default:
-                        break;
+                    nextMove = threeMoves(generator);
+                    switch(nextMove) 
+                    {
+                        case 0: /* right */
+                            ant.x ++;
+                            break;
+                        case 1: /* up */
+                            ant.y ++;
+                            break;
+                        case 2: /* down */
+                            ant.y --;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else if (ant.x == 4)
+                {
+                    nextMove = threeMoves(generator);
+                    switch(nextMove) 
+                    {
+                        case 0: /* left */
+                            ant.x --;
+                            break;
+                        case 1: /* up */
+                            ant.y ++;
+                            break;
+                        case 2: /* down */
+                            ant.y --;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else 
+                {
+                    nextMove = fourMoves(generator);
+                    switch(nextMove) 
+                    {
+                        case 0: /* up */
+                            ant.y ++;
+                            break;
+                        case 1: /* down */
+                            ant.y --;
+                            break;
+                        case 2: /* left */
+                            ant.x --;
+                            break;
+                        case 3: /* right */
+                            ant.x ++;
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
         }
-
+        
         totalMoves += ant.numMoves;
     }
 }
@@ -269,17 +250,20 @@ void laboriousAnt(const unsigned numRuns)
 */
 int main(int argc, char* argv[]) 
 {
+    /* measure execution time - start */
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
+
     /* determine number of threads system can support */
-    const unsigned numThreads = thread::hardware_concurrency();
+    const unsigned numThreads = thread::hardware_concurrency() - 1;
 
     /* number of runs per thread */
-    const unsigned numRuns = strtoul(argv[1], NULL, 10);
-
-    /* expected number of runs - used for looping until convergence is reached */
-    double expectedMoves;
+    unsigned numRuns = ceil((double) 10000000 / numThreads);
 
     /* total number of runs across all threads */
-    const unsigned totalRuns = numRuns * numThreads;
+    unsigned totalRuns;
+
+    /* expected number of runs */
+    double expectedMoves;
 
     /* create array of threads and instantiate each thread */
     thread threads[numThreads];
@@ -294,12 +278,22 @@ int main(int argc, char* argv[])
         threads[i].join();
     }
 
-    expectedMoves = (double) totalMoves / totalRuns;
+    totalRuns = (numRuns * numThreads);
+
+    /* calculate expected moves */
+    expectedMoves = ((double) totalMoves) / ((double) totalRuns);
+
+    /* measure execution time - end */
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+
+    /* print time elapsed */
+    const int64_t elapsed = chrono::duration_cast<chrono::seconds>(end - start).count();
+    cout << "Time elapsed: " << elapsed << " seconds" << endl;
 
     /* write output to file */
     ofstream ofs ("ProblemOne.txt", ofstream::trunc);
-    ofs << "Number of threads created: " << numThreads << endl << endl;
-    ofs << "Expected number of steps: " << setprecision(10) << expectedMoves << endl << endl;
+    ofs << "Number of threads created: " << (numThreads + 1) << endl << endl;
+    ofs << "Expected number of steps: " << setprecision(9) << expectedMoves << endl << endl;
     ofs << "Total number of runs needed for solution convergence: " << totalRuns << endl;
     ofs.close();
 
