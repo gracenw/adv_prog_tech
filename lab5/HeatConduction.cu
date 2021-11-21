aa43c11a99e0dad15da173c1e6f9dedb20638967
gracenw <gracen.wallace@gatech.edu>
2021-11-21 15:11:35 -0500

    lab 5 almost done

--- lab5/HeatConduction.cu
+++ lab5/HeatConduction.cu
@@ -1,21 +1,20 @@
 #include <stdio.h>
-#include <cuda_runtime.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <iostream>
 #include <iomanip>
 #include <unistd.h>
 #include <fstream>
+#include <cuda_runtime.h>
 
 #define MAX_BOUNDARY    100
 #define INIT_TEMP       20
 
 /* 
   function: checkArgs
-  ensures the command line argument fits requirements, 
-  saves unsigned in placeholder and aborts otherwise
+  ensures the command line argument fits requirements
 */
-void checkArgs(const char * arg, unsigned & placeholder) 
+unsigned checkArgs(const char * arg) 
 {
     /* check that all chars in arg are numbers */
     for (int i = 0; i < strlen(arg); i++) 
@@ -23,50 +22,46 @@ void checkArgs(const char * arg, unsigned & placeholder)
         if (int(arg[i]) < 48 || int(arg[i]) > 57) 
         {
             std::cout << "Invalid parameters, please check your values." << std::endl;
-            abort();
+            exit(EXIT_FAILURE);
         }
     }
 
     /* convert to unsigned long */
-    placeholder = strtoul(arg, NULL, 10);
+    unsigned val = strtoul(arg, NULL, 10);
 
     /* check that unsigned long is not zero or negative */
-    if (placeholder <= 0) 
+    if (val <= 0) 
     {
         std::cout << "Invalid parameters, please check your values." << std::endl;
-        abort();
+        exit(EXIT_FAILURE);
     }
+
+    return val;
 }
 
 /* 
   function: calculateTemp
   
 */
-__global__ void calculateTemp(double * h, unsigned N, unsigned I)
+__global__ void calculateTemp(double * h, double * g, unsigned N)
 {
-    while (I > 0)
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if ((i % (N - 1)) != 0 && (i % N) != 0 && i > N && i < (N * (N - 1)))
     {
-        double * g = (double *) malloc(N * N * sizeof(double));
-
-        for (int i = 1; i < (N - 1); i++)
-        {
-            for (int j = 1; j < (N - 1); j++)
-            {
-                *(g + (j * N) + i) = 0.25 * *(h + (j * N) + i - 1) + *(h + (j * N) + i + 1) + *(h + ((j - 1) * N) + i) + *(h + ((j + 1) * N) + i);
-            }
-        }
-
-        for (int i = 1; i < (N - 1); i++)
-        {
-            for (int j = 1; j < (N - 1); j++)
-            {
-                *(h + (j * N) + i) = *(g + (j * N) + i);
-            }
-        }
-
-        free(g);
-        I--;
+        g[i] = 0.25 * (h[i- 1] + h[i + 1] + h[i - N] + h[i + N]);
     }
+}
+
+/* 
+  function: copyMatrix
+  
+*/
+__global__ void copyMatrix(double * h, double * g, unsigned N)
+{
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    h[i] = g[i];
 }
 
 /* function: main
@@ -75,93 +70,142 @@ __global__ void calculateTemp(double * h, unsigned N, unsigned I)
 int main(int argc, char * argv[])
 {
     /* check arguments */
-    unsigned N;
-    unsigned I;
-    int c;
-    while ((c = getopt(argc, argv, "NI:")) != -1)
-    {
-        switch (c)
-        {
-            case 'N':
-                checkArgs(optarg, N);
-                N = N + 2;
-                break;
-            case 'I':
-                checkArgs(optarg, I);
-                break;
-            case '?':
-                std::cout << "Invalid parameters, please check your values." << std::endl;
-                return 1;
-            default:
-                break;
-        }
-    }
+    unsigned N = 100;
+    unsigned I = 1;
+
+    // for(int i = 1; i < argc; i++)
+    // {
+
+    // }
+    
+    /* placeholder for error checking */
+    cudaError_t err = cudaSuccess;
 
     /* allocate cpu mem to array */
     size_t size = N * N * sizeof(double);
+    int numElements = (N - 2) * (N - 2);
     double * H_h = (double *) malloc(size);
+    double * H_g = (double *) malloc(size);
 
-    /* initialize array interior values */
-    for (int i = 1; i < (N - 1); i++)
+    /* verify that allocation succeeded */
+    if (H_h == NULL || H_g == NULL) 
     {
-        for (int j = 1; j < (N - 1); j++)
-        {
-            *(H_h + (j * N) + i) = INIT_TEMP;
-        }
+        std::cout << "Failed to allocate host matrices" << std::endl;
+        exit(EXIT_FAILURE);
     }
 
-    /* initialize array exterior values */
-    for (int i = 0; i < N; i++)
+    /* initialize all array interior positions */
+    for (int i = 0; i < (N * N - 1); i++)
     {
         if (i > (0.3 * (N - 1)) && i < (0.7 * (N - 1)))
         {
-            *(H_h + i) = MAX_BOUNDARY;
+            H_h[i] = MAX_BOUNDARY;
+            H_g[i] = MAX_BOUNDARY;
         }
         else
         {
-            *(H_h + i) = INIT_TEMP;
+            H_h[i] = INIT_TEMP;
+            H_g[i] = INIT_TEMP;
         }
-        *(H_h + ((N - 1) * N) + i) = INIT_TEMP;
     }
-    for (int j = 1; j < (N - 1); j++)
+
+    /* set small section to 100 celsius */
+    for (int i = ((int) ); i < ((int) ); i++)
     {
-        *(H_h + (j * N)) = INIT_TEMP;
-        *(H_h + (j * N) + N - 1) = INIT_TEMP;
+        
     }
 
-    /* get device properties to set num of threads and blocks */
-    cudaDeviceProp properties;
-    cudaGetDeviceProperties(&properties, 0);
-    int maxThreadsPerBlock = properties.maxThreadsPerBlock;
-
-    /* allocate gpu mem to array */
+    /* allocate gpu mem to matrix H */
     double * D_h = NULL;
-    cudaMalloc((void **) &D_h, size);
+    err = cudaMalloc((void **) &D_h, size);
 
-    /* send cpu data to gpu */
-    cudaMemcpy(D_h, H_h, size, cudaMemcpyHostToDevice);
+    // /* verify that allocation succeeded */
+    // if (err != cudaSuccess) 
+    // {
+    //     std::cout << "Failed to allocate device matrix H - " << cudaGetErrorString(err) << std::endl;
+    //     exit(EXIT_FAILURE);
+    // }
+
+    /* allocate gpu mem to matrix G */
+    double * D_g = NULL;
+    err = cudaMalloc((void **) &D_g, size);
+
+    // /* verify that allocation succeeded */
+    // if (err != cudaSuccess) 
+    // {
+    //     std::cout << "Failed to allocate device matrix G - " << cudaGetErrorString(err) << std::endl;
+    //     exit(EXIT_FAILURE);
+    // }
+
+    /* send matrix H to gpu */
+    err = cudaMemcpy(D_h, H_h, size, cudaMemcpyHostToDevice);
+
+    // /* verify memcopy from host to device succeeded */
+    // if (err != cudaSuccess) 
+    // {
+    //     std::cout << "Failed to copy matrix H from host to device - " << cudaGetErrorString(err) << std::endl;
+    //     exit(EXIT_FAILURE);
+    // }
+
+    /* send matrix G to gpu */
+    err = cudaMemcpy(D_g, H_g, size, cudaMemcpyHostToDevice);
+
+    // /* verify memcopy from host to device succeeded */
+    // if (err != cudaSuccess) 
+    // {
+    //     std::cout << "Failed to copy matrix G from host to device - " << cudaGetErrorString(err) << std::endl;
+    //     exit(EXIT_FAILURE);
+    // }
 
     /* initialize event timing variables */
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
-    /* execute kernel with timing */
+    /* get device properties to set num of threads and blocks */
+    cudaDeviceProp properties;
+    cudaGetDeviceProperties(&properties, 0);
+    int threadsPerBlock = properties.maxThreadsPerBlock;
+    int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock;
+
+    /* execute kernel with in loop with timing */
     cudaEventRecord(start);
-    calculateTemp<<<1, maxThreadsPerBlock>>>(D_h, N, I);
+    while(I > 0)
+    {
+        calculateTemp<<<blocksPerGrid, threadsPerBlock>>>(D_h, D_g, N);
+        cudaDeviceSynchronize();
+        copyMatrix<<<blocksPerGrid, threadsPerBlock>>>(D_h, D_g, N);
+        cudaDeviceSynchronize();
+        I--;
+    }
     cudaEventRecord(stop);
 
-    /* synchronize gpu & event at end of kernel execution */
-    cudaDeviceSynchronize();
+    /* synchronize event at end of kernel execution */
     cudaEventSynchronize(stop);
+
+    // /* verify successful launch */
+    // err = cudaGetLastError();
+
+    // if (err != cudaSuccess) 
+    // {
+    //     std::cout << "Failed to launch calculateTemp kernel - " << cudaGetErrorString(err) << std::endl;
+    //     exit(EXIT_FAILURE);
+    // }
 
     /* calculate kernel execution time & write to console */
     float milli = 0;
     cudaEventElapsedTime(&milli, start, stop);
-    std::cout << std::fixed << std::setprecision(2) << milli;
+    std::cout << std::fixed << std::setprecision(2) << milli << std::endl;
 
     /* copy data back to cpu */
-    cudaMemcpy(H_h, D_h, size, cudaMemcpyDeviceToHost);
+    err = cudaMemcpy(H_h, D_h, size, cudaMemcpyDeviceToHost);
+
+    // /* verify memcopy from device to host succeeded */
+    // if (err != cudaSuccess) 
+    // {
+    //     std::cout << "Failed to copy matrix H from device to host - " << cudaGetErrorString(err) << std::endl;
+    //     exit(EXIT_FAILURE);
+    // }
 
     /* evaluate results & write to csv */
     std::ofstream ofs ("finalTemperatures.csv", std::ofstream::trunc);
@@ -169,16 +213,36 @@ int main(int argc, char * argv[])
     {
         for (int j = 0; j < N; j++)
         {
-            ofs << std::fixed << std::setprecision(8) << *(H_h + (j * N) + i) << ',';
+            ofs << std::fixed << std::setprecision(6) << *(H_h + (j * N) + i) << ", ";
         }
         ofs << std::endl;
     }
     ofs.close();
 
-    /* free allocated memory */
-    cudaFree(D_h);
-    free(H_h);
+    /* free device memory */
+    err = cudaFree(D_h);
     
+    // /* verify cuda memory successfully freed */
+    // if (err != cudaSuccess) 
+    // {
+    //     std::cout << "Failed to free device matrix/matrices - " << cudaGetErrorString(err) << std::endl;
+    //     exit(EXIT_FAILURE);
+    // }
+
+    /* free device memory */
+    err = cudaFree(D_g);
+    
+    // /* verify cuda memory successfully freed */
+    // if (err != cudaSuccess) 
+    // {
+    //     std::cout << "Failed to free device matrix/matrices - " << cudaGetErrorString(err) << std::endl;
+    //     exit(EXIT_FAILURE);
+    // }
+    
+    /* free host memory */
+    free(H_h);
+    free(H_g);
+
     /* return successfully */
-    return 0;
+    exit(EXIT_SUCCESS);
 }
